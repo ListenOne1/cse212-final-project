d7f8e682f4a5b4c8108788052ea0087bf1391a5b
ListenOne1 <91183626+ListenOne1@users.noreply.github.com>
2022-04-05 19:41:55 -0600

    Add files via upload

---
+++ LL/linkedlistAnswer.py
@@ -0,0 +1,18 @@
+item = {"eggs","green cabbage","ground beef","onion","cucumber"}
+###############
+# Start there #
+###############
+
+item.remove("eggs")
+item.remove("ground beef")
+item.remove("cucumber")
+item.add("rice")
+item.add("apple")
+
+#######
+# End #
+#######
+
+print(f"This is my new shopping list. {item}")
+# This is my new shopping list. {'rice', 'apple', 'green cabbage', 'onion'}
+# The output might be different.
